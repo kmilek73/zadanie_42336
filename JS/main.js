1303ab9f975d03e13fd808736dc2f147b769abb3
kmilek <kmilek73@gmail.com>
2022-12-05 22:53:24 +0100

    rozwiazanie

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+
+let zmiana = document.querySelector('p');
+
+document.querySelector("button[name=wstaw]").onclick = function() {
+(zmiana.textContent ='ZMANIENIONy TEKST');
+}
+
+let czysc = document.querySelector('p');
+document.querySelector("button[name=wyczysc]").onclick = function() {
+(czysc.textContent ='');
+
+}
+
